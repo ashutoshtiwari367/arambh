9aeec9dd9623a262aeeab4ea7a7d87369e792877
ashutoshtiwari367 <ashutoshtiwari9453@gmail.com>
2023-11-22 23:00:12 -0500

    add

---
+++ student.php
@@ -0,0 +1,60 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './phpMailer/src/Exception.php';
+require './phpMailer/src/PHPMailer.php';
+require './phpMailer/src/SMTP.php';
+
+
+$parent_name = $_POST['parent_name'];
+$email = $_POST['email'];
+
+$mail = new PHPMailer(true);
+    
+$mail->isSMTP();
+// $mail->Host = 'smtp.gmail.com';
+// $mail->Host = 'smtp.titan.email';
+$mail->Host = 'smtp.hostinger.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'info@aarambhtutorials.com';
+$mail->Password = 'Shivam@134';
+$mail->SMTPSecure = 'ssl';
+$mail->Port= 465;
+// $mail->Host = 'smtp.gmail.com';
+// $mail->SMTPAuth = true;
+// $mail->Username = 'deepakbaradwaj933@gmail.com';
+// $mail->Password = 'sjkosmmlnbabblmm';
+// $mail->SMTPSecure = 'ssl';
+// $mail->Port= 465;
+// 
+$mail->setFrom('info@aarambhtutorials.com', 'deepak');
+
+
+$mail->addAddress($email);
+$mail->addBCC('ashutoshtiwari9453@gmail.com', 'new customer details');
+
+$mail->isHTML(true);
+
+$mail->Subject = 'Qoute from Arambh';
+
+$mail->Body =  '
+<table>
+<tr style="">
+<td>'.$parent_name.' asshhytjgktjtjrdjrtjf rdrjt</td>
+</tr>
+</table>
+
+
+';
+
+
+
+
+if(!$mail->send()){
+    echo json_encode(['status'=>500]);
+   }else{
+       echo json_encode(['status'=>200]);
+   }
+   ?>
