8134a2710c604c4ffcf566c562bdc562560e0db6
ashutoshtiwari367 <ashutoshtiwari9453@gmail.com>
2023-11-23 12:25:06 -0500

    ass

--- student.php
+++ student.php
@@ -9,8 +9,12 @@ require './phpMailer/src/SMTP.php';
 
 
 $parent_name = $_POST['parent_name'];
+$student_name = $_POST['student_name'];
+$class_ = $_POST['class_'];
+$School_name = $_POST['School_name'];
+$address_ = $_POST['address_'];
+$phone_no = $_POST['phone_no'];
 $email = $_POST['email'];
-
 $mail = new PHPMailer(true);
     
 $mail->isSMTP();
@@ -40,12 +44,57 @@ $mail->isHTML(true);
 $mail->Subject = 'Qoute from Arambh';
 
 $mail->Body =  '
-<table>
-<tr style="">
-<td>'.$parent_name.' Thank you for fill the form .
-We will call you as soon as possible.</td>
-</tr>
-</table>
+<body style="background-color: black;">
+    <div><h1 style="text-align: center; background-color: black; color: white; height: auto wid
+    80%; padding: 3%;">ARAMBH</h1></div>
+    <table style="margin: auto; align-items:center; justify-content: center;">
+        <tr style=" font-size: larger; color: white;">
+        <td> Thank you for fill the form .
+        We will call you as soon as possible.</td>
+        </tr>
+        </table>
+        <h1 style="color: white; text-align: center;">Your details</h1>
+        <table style="margin: auto; width: 100%; text-align: center; font-size: larger;">
+            <tbody style="color: white;">
+            <tr>
+                <td>Parents name</td>
+                <td>'.$parent_name.'</td>
+              
+            </tr>
+            <tr>
+                <td>Student name</td>
+                <td>'.$student_name.'</td>
+              
+            </tr>
+            <tr>
+                <td>Class</td>
+                <td>'.$class_.'</td>
+              
+            </tr>
+            <tr>
+                <td>School</td>
+                <td>'.$School_name.'</td>
+              
+            </tr>
+            <tr>
+                <td>Address</td>
+                <td>'.$address_.'</td>
+              
+            </tr>
+            <tr>
+                <td>Phone no</td>
+                <td>'.$phone_no.'</td>
+              
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>'.$email.'</td>
+              
+            </tr>
+            </tbody>
+        </table>
+        <p style="color: white; text-align: center;">Thank you</p>
+</body>
 
 
 ';
